f217f3f5be4fd7fa0696ec16e191ca6ef9b79da8
Tamas Pentek <pentek_pityu@yahoo.com>
2020-11-09 13:56:44 +0200

    Add intitial project

---
+++ src/main/java/ActivityReaderApplication.java
@@ -0,0 +1,13 @@
+public class ActivityReaderApplication {
+
+
+    public ActivityReaderApplication() {
+    }
+
+
+    public static void main(String args[]) {
+
+        System.out.println("The activity reader is running");
+
+    }
+}
